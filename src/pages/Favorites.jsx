8f48ad49b04acb980eec75259a72dc12fd1ea0ad
RichardAcharya <narutouzu910@gmail.com>
2026-04-23 11:40:38 +0545

    Initail commit

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+import "../css/Favorites.css"
+function Favorites() {
+    return <div className="favorites-empty">
+        <h2>No Favorite Movie Yet</h2>
+        <p>Start adding more movies to your favourites and they will appear her </p>
+    </div>
+}
+
+export default Favorites

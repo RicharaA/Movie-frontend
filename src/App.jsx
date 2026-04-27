596a29655c98f48203a62f460db7ccb5824dd0bb
RichardAcharya <narutouzu910@gmail.com>
2026-04-27 13:10:11 +0545

    Full website

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./css/App.css";
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact.jsx"
 import Favorites from "./pages/Favorites";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer.jsx"
-import Contact from "./pages/Contact.jsx"
+
 
 function App() {
   return (
